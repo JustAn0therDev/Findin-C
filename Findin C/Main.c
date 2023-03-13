e3866f27fd8c44e4972ea5e03e4d26fa0460d4be
Ruan Montelo <ruanmontelo@gmail.com>
2023-03-13 18:58:06 -0300

    Changed all string.h function calls for secure ones in Windows and changed indentation style for Emacs

--- Findin C/Main.c
+++ Findin C/Main.c
@@ -3,8 +3,6 @@
 #include <string.h>
 #include <Windows.h>
 
-#pragma warning(disable : 4996)
-
 size_t FindInCurrentDirectory(char* path, char* search, char* fileExtension, char* currentSubDirectory);
 size_t FindIn(char* search, char* filePath, char* subDirectoryToShow);
 inline int EndsWith(char* str, char* subStr);
@@ -12,234 +10,237 @@ inline char* GetInnermostFileNameInPath(char* filePath);
 
 int main(int argc, char** argv) {
 #ifndef DEBUG
-	if (argc != 3) {
-		printf("A file extension and a search argument must be present.");
-		return 1;
-	}
+  if (argc != 3) {
+    printf("A file extension and a search argument must be present.");
+    return 1;
+  }
 
-	char* fileExtension = *(argv + 1);
-	char* search = *(argv + 2);
+  char* fileExtension = *(argv + 1);
+  char* search = *(argv + 2);
 
-	char currentDirectoryBuffer[MAX_PATH];
+  char currentDirectoryBuffer[MAX_PATH];
 
-	unsigned long bytesWritten = GetCurrentDirectoryA(MAX_PATH, currentDirectoryBuffer);
+  unsigned long bytesWritten = GetCurrentDirectoryA(MAX_PATH, currentDirectoryBuffer);
 
-	if (bytesWritten == 0) {
-		printf("Couldn't find current directory");
-		return 1;
-	}
+  if (bytesWritten == 0) {
+    printf("Couldn't find current directory");
+    return 1;
+  }
 #else
-	char* fileExtension = ".cs";
-	char* search = "o";
+  char* fileExtension = ".c";
+  char* search = "void";
 
-	char currentDirectoryBuffer[MAX_PATH];
+  char currentDirectoryBuffer[MAX_PATH];
+  char* path = "D:\\repos";
 
-	strcpy(currentDirectoryBuffer, "D:\\repos");
+  strncpy_s(currentDirectoryBuffer, MAX_PATH, path, strlen(path));
 #endif
-	char* innermostDirectory = GetInnermostFileNameInPath(currentDirectoryBuffer);
+  char* innermostDirectory = GetInnermostFileNameInPath(currentDirectoryBuffer);
 
-	size_t occurrences = FindInCurrentDirectory(currentDirectoryBuffer, search, fileExtension, innermostDirectory);
+  size_t occurrences = FindInCurrentDirectory(currentDirectoryBuffer, search, fileExtension, innermostDirectory);
 
-	printf("Found a total of %zd occurrences.\n", occurrences);
+  printf("Found a total of %zd occurrences.\n", occurrences);
 
-	return 0;
+  return 0;
 }
 
 size_t FindInCurrentDirectory(char* path, char* search, char* fileExtension, char* currentSubDirectory) {
-	WIN32_FIND_DATA w32fd = { 0 };
-	HANDLE hFind;
+  WIN32_FIND_DATA w32fd = { 0 };
+  HANDLE hFind;
 
-	char directoryBufferForLookup[MAX_PATH];
-	char directoryBuffer[MAX_PATH];
+  char directoryBufferForLookup[MAX_PATH];
+  char directoryBuffer[MAX_PATH];
 
-	strcpy(directoryBufferForLookup, path);
+  strncpy_s(directoryBufferForLookup, MAX_PATH, path, strlen(path));
 
-	strcpy(directoryBuffer, directoryBufferForLookup);
+  strncpy_s(directoryBuffer, MAX_PATH, directoryBufferForLookup, MAX_PATH);
 
-	strcat(directoryBufferForLookup, "\\");
-	strcat(directoryBufferForLookup, "*.*");
+  strncat_s(directoryBufferForLookup, MAX_PATH, "\\", 2);
+  strncat_s(directoryBufferForLookup, MAX_PATH, "*.*", 3);
 
-	if ((hFind = FindFirstFileA(directoryBufferForLookup, (LPWIN32_FIND_DATAA)&w32fd)) == INVALID_HANDLE_VALUE) {
-		printf("Couldn't find any files with this extension");
-		exit(1);
-	}
+  if ((hFind = FindFirstFileA(directoryBufferForLookup, (LPWIN32_FIND_DATAA)&w32fd)) == INVALID_HANDLE_VALUE) {
+    printf("Couldn't find any files with this extension");
+    exit(1);
+  }
 
-	size_t totalOccurrences = 0;
+  size_t totalOccurrences = 0;
 
-	do {
-		char* fileName = (char*)w32fd.cFileName;
+  do {
+    char* fileName = (char*)w32fd.cFileName;
 
-		if (strcmp(".", fileName) == 0 || strcmp("..", fileName) == 0) {
-			continue;
-		}
+    if (strcmp(".", fileName) == 0 || strcmp("..", fileName) == 0) {
+      continue;
+    }
 
-		char subDirectoryToShow[MAX_PATH] = { 0 };
+    char subDirectoryToShow[MAX_PATH] = { 0 };
 
-		if (currentSubDirectory) {
-			strcpy(subDirectoryToShow, currentSubDirectory);
-			strcat(subDirectoryToShow, "/");
-			strcat(subDirectoryToShow, fileName);
-		}
-		else {
-			strcpy(subDirectoryToShow, fileName);
-		}
+    if (currentSubDirectory) {
+      strncpy_s(subDirectoryToShow, MAX_PATH, currentSubDirectory, strlen(currentSubDirectory));
+      strncat_s(subDirectoryToShow, MAX_PATH, "/", 1);
+      strncat_s(subDirectoryToShow, MAX_PATH, fileName, strlen(fileName));
+    }
+    else {
+      strncpy_s(subDirectoryToShow, MAX_PATH, fileName, strlen(fileName));
+    }
 
-		if (w32fd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
-			char subDirectoryPath[MAX_PATH] = { 0 };
+    if (w32fd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
+      char subDirectoryPath[MAX_PATH] = { 0 };
 
-			strcpy(subDirectoryPath, directoryBuffer);
-			strcat(subDirectoryPath, "\\");
-			strcat(subDirectoryPath, fileName);
+      strncpy_s(subDirectoryPath, MAX_PATH, directoryBuffer, strlen(directoryBuffer));
+      strncat_s(subDirectoryPath, MAX_PATH, "\\", 2);
+      strncat_s(subDirectoryPath, MAX_PATH, fileName, strlen(fileName));
 
-			totalOccurrences += FindInCurrentDirectory(subDirectoryPath, search, fileExtension, subDirectoryToShow);
-		}
-		else if (w32fd.dwFileAttributes & FILE_ATTRIBUTE_ARCHIVE && EndsWith(fileName, fileExtension)) {
-			char path[MAX_PATH] = { 0 };
+      totalOccurrences += FindInCurrentDirectory(subDirectoryPath, search, fileExtension, subDirectoryToShow);
+    }
+    else if (w32fd.dwFileAttributes & FILE_ATTRIBUTE_ARCHIVE && EndsWith(fileName, fileExtension)) {
+      char path[MAX_PATH] = { 0 };
 
-			strcpy(path, directoryBuffer);
+      strncpy_s(path, MAX_PATH, directoryBuffer, strlen(directoryBuffer));
 
-			strcat(path, "\\");
-			strcat(path, fileName);
+      strncat_s(path, MAX_PATH, "\\", 2);
+      strncat_s(path, MAX_PATH, fileName, strlen(fileName));
 
-			totalOccurrences += FindIn(search, path, subDirectoryToShow);
-		}
-	} while (FindNextFileA(hFind, (LPWIN32_FIND_DATAA)&w32fd));
+      totalOccurrences += FindIn(search, path, subDirectoryToShow);
+    }
+  } while (FindNextFileA(hFind, (LPWIN32_FIND_DATAA)&w32fd));
 
-	FindClose(hFind);
+  FindClose(hFind);
 
-	return totalOccurrences;
+  return totalOccurrences;
 }
 
 size_t FindIn(char* search, char* filePath, char* subDirectoryToShow) {
-	FILE* fp = fopen(filePath, "r");
+  FILE* fp = 0;
 
-	if (!fp) {
-		printf("Couldn't open file: %s\n", filePath);
-		return 0;
-	}
+  if (fopen_s(&fp, filePath, "r") != 0) {
+    printf("Couldn't open file: %s\n", filePath);
+    return 0;
+  }
 
-	rewind(fp);
+  rewind(fp);
 
-	size_t charIndex = 0;
-	size_t lineNumber = 1;
-	char c;
-	char* line = 0;
-	size_t lineSize = 0;
-	size_t searchIndex = 0;
-	size_t lastSearchIndexFound = 0;
-	size_t foundInLine = 0;
-	size_t occurrences = 0;
+  size_t charIndex = 0;
+  size_t lineNumber = 1;
+  char c;
+  char* line = 0;
+  size_t lineSize = 0;
+  size_t searchIndex = 0;
+  size_t lastSearchIndexFound = 0;
+  size_t foundInLine = 0;
+  size_t occurrences = 0;
 
-	while ((c = fgetc(fp)) != EOF) {
-		if (lineSize == 0 && (c == ' ' || c == '\t')) {
-			charIndex++;
-			continue;
-		}
+  while ((c = fgetc(fp)) != EOF) {
+    if (lineSize == 0 && (c == ' ' || c == '\t')) {
+      charIndex++;
+      continue;
+    }
 
-		// A char of the string was found, the searchIndex is smaller than the size
-		// of the search string (didn't find everything yet), AND 
-		// (the currentIndex is + 1 bigger than the last one OR it's the first char)
-		if (search[searchIndex] == c && searchIndex < strlen(search) &&
-			(charIndex == (lastSearchIndexFound + 1) || lastSearchIndexFound == 0)) {
-			lastSearchIndexFound = charIndex;
-			searchIndex++;
-		}
-		else { // Nothing was found for now, so reset the state of the indices.
-			searchIndex = 0;
-			lastSearchIndexFound = 0;
-		}
+    // A char of the string was found, the searchIndex is smaller than the size
+    // of the search string (didn't find everything yet), AND 
+         // (the currentIndex is + 1 bigger than the last one OR it's the first char)
+         if (search[searchIndex] == c && searchIndex < strlen(search) &&
+             (charIndex == (lastSearchIndexFound + 1) || lastSearchIndexFound == 0)) {
+           lastSearchIndexFound = charIndex;
+           searchIndex++;
+         }
+         else { // Nothing was found for now, so reset the state of the indices.
+           searchIndex = 0;
+           lastSearchIndexFound = 0;
+         }
 
-		// The searchIndex is now of the same size as the string.
-		// Reset the state and look for more occurrences in the same line.
-		if (searchIndex == strlen(search)) {
-			foundInLine++;
-			searchIndex = 0;
-		}
+    // The searchIndex is now of the same size as the string.
+    // Reset the state and look for more occurrences in the same line.
+    if (searchIndex == strlen(search)) {
+      foundInLine++;
+      searchIndex = 0;
+    }
 
-		lineSize++;
+    lineSize++;
 
-		if (line == 0) {
-			line = malloc(sizeof(c) + 1);
+    char toAppend[2] = { c, '\0'};
 
-			if (line == 0) {
-				exit(1);
-			}
+    if (line == 0) {
+      line = malloc(sizeof(c) * 2);
 
-			*(line + lineSize) = '\0';
+      if (line == 0) {
+        exit(1);
+      }
 
-			strncpy(line, &c, 1);
-		}
-		else {
-			char* oldLineBuffer = line;
-			line = realloc(line, sizeof(c) * (lineSize + 1));
+      *(line + lineSize) = '\0';
 
-			if (line == 0) {
-				free(oldLineBuffer);
-				printf("Realloc failed.\n");
-				exit(1);
-			}
+      strncpy_s(line, lineSize + 1, toAppend, 1);
+    }
+    else {
+      char* oldLineBuffer = line;
+      line = realloc(line, sizeof(c) * (lineSize + 1));
 
-			strncat(line, &c, 1);
-		}
+      if (line == 0) {
+        free(oldLineBuffer);
+        printf("Realloc failed.\n");
+        exit(1);
+      }
 
-		if (c == '\n') {
-			lineNumber++;
+      strncat_s(line, lineSize + 1, toAppend, 1);
+    }
 
-			if (foundInLine) {
-				occurrences += foundInLine;
-				char* fileNameFromPath = GetInnermostFileNameInPath(filePath);
-				printf("[%s] at line %zd: %s", subDirectoryToShow, lineNumber, line);
-			}
+    if (c == '\n') {
+      lineNumber++;
 
-			// Reset the state of everything because we count info per line.
-			free(line);
-			line = 0;
-			lineSize = 0;
-			searchIndex = 0;
-			lastSearchIndexFound = 0;
-			foundInLine = 0;
-		}
+      if (foundInLine) {
+        occurrences += foundInLine;
+        char* fileNameFromPath = GetInnermostFileNameInPath(filePath);
+        printf("[%s] at line %zd: %s", subDirectoryToShow, lineNumber, line);
+      }
 
-		charIndex++;
-	}
+      // Reset the state of everything because we count info per line.
+      free(line);
+      line = 0;
+      lineSize = 0;
+      searchIndex = 0;
+      lastSearchIndexFound = 0;
+      foundInLine = 0;
+    }
 
-	if (line) {
-		free(line);
-	}
+    charIndex++;
+  }
 
-	fclose(fp);
+  if (line) {
+    free(line);
+  }
 
-	return occurrences;
+  fclose(fp);
+
+  return occurrences;
 }
 
 int EndsWith(char* c, char* subStr) {
-	int subStrIndex = (int)strlen(subStr) - 1;
-	char* subStrEnd = subStr + (strlen(subStr) - 1);
-	char* cEnd = c + (strlen(c) - 1);
+  int subStrIndex = (int)strlen(subStr) - 1;
+  char* subStrEnd = subStr + (strlen(subStr) - 1);
+  char* cEnd = c + (strlen(c) - 1);
 
-	while (subStrIndex > -1) {
-		if (*(subStrEnd) != (*cEnd)) {
-			return 0;
-		}
+  while (subStrIndex > -1) {
+    if (*(subStrEnd) != (*cEnd)) {
+      return 0;
+    }
 
-		subStrEnd--;
-		cEnd--;
-		subStrIndex--;
-	}
+    subStrEnd--;
+    cEnd--;
+    subStrIndex--;
+  }
 
-	return 1;
+  return 1;
 }
 
 char* GetInnermostFileNameInPath(char* path) {
-	if (path == 0) {
-		return 0;
-	}
+  if (path == 0) {
+    return 0;
+  }
 
-	char* pathPtr = path + strlen(path);
+  char* pathPtr = path + strlen(path);
 
-	while (*pathPtr != '\\') {
-		pathPtr--;
-	}
+  while (*pathPtr != '\\') {
+    pathPtr--;
+  }
 
-	return pathPtr + sizeof(*path);
+  return pathPtr + sizeof(*path);
 }
